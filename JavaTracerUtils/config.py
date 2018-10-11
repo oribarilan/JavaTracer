e528def07165574497061a9f289c3474fb5af833
ori <ori@zeitgold.com>
2018-10-11 14:04:26 +0300

    config changes and added notebook for autoencoder

--- JavaTracerUtils/config.py
+++ JavaTracerUtils/config.py
@@ -7,7 +7,7 @@ class Config:
     # this requires complete names: <package>.<class>.<method>(parameters)
     # example: 'fraction.BigFraction.BigFraction(double_double_int_int)'
     # another example: 'org.apache.commons.lang3.math.NumberUtils.createNumber(java.lang.String)'
-    actual_faults_method_fullname_set = frozenset({'org.apache.commons.lang3.toLocale(java.lang.String)'})
+    actual_faults_method_fullname_set = frozenset({'org.apache.commons.lang3.LocaleUtils.toLocale(java.lang.String)'})
     ON_THE_FLY = True  # don't create intermediate files -> this improves performance
     # endregion
 
